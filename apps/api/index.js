810a73d691ab8c94fe50696b643fef4052acb4b5
Luis Dourado <lfaddourado@gmail.com>
2023-03-05 11:27:50 -0300

    feat: add post and delete methods

--- apps/api/index.js
+++ apps/api/index.js
@@ -6,13 +6,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const data_json_1 = __importDefault(require("./data.json"));
 const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const ajv_1 = __importDefault(require("ajv"));
 const app = (0, express_1.default)();
+const ajv = new ajv_1.default();
+const userSchema = {
+    type: 'object',
+    properties: {
+        id: { type: 'number' },
+        name: { type: 'string' },
+        email: { type: 'string', format: 'email' },
+    },
+    required: ['name', 'email'],
+    additionalProperties: false,
+};
+const validate = ajv.compile(userSchema);
 const corsOptions = {
     origin: '*',
 };
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 app.use((0, cors_1.default)(corsOptions));
-app.get('/', (req, res) => {
+app.use(body_parser_1.default.json());
+app.get('/users', (req, res) => {
     res.send(data_json_1.default.users.map((user) => {
         return {
             id: user.id,
@@ -21,6 +36,29 @@ app.get('/', (req, res) => {
         };
     }));
 });
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    const valid = validate(newUser);
+    if (!valid) {
+        res.status(400).json({ error: validate.errors });
+    }
+    else {
+        newUser.id = data_json_1.default.users.length + 1;
+        data_json_1.default.users.push(newUser);
+        res.status(201).json(newUser);
+    }
+});
+app.delete('/users/:id', (req, res) => {
+    const userId = Number(req.params.id);
+    const userIndex = data_json_1.default.users.findIndex((user) => user.id === userId);
+    if (userIndex === -1) {
+        res.status(404).json({ error: 'User not found' });
+    }
+    else {
+        data_json_1.default.users.splice(userIndex, 1);
+        res.status(204).send();
+    }
+});
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:3000`);
+    console.log(`Server started at ${port}`);
 });
--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,7 +1,23 @@
 import express from 'express';
 import data from './data.json';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import Ajv from 'ajv';
 const app = express();
+const ajv = new Ajv();
+
+const userSchema = {
+  type: 'object',
+  properties: {
+    id: { type: 'number' },
+    name: { type: 'string' },
+    email: { type: 'string', format: 'email' },
+  },
+  required: ['name', 'email'],
+  additionalProperties: false,
+};
+
+const validate = ajv.compile(userSchema);
 
 const corsOptions = {
   origin: '*',
@@ -10,8 +26,9 @@ const corsOptions = {
 const port = process.env.PORT || 8000;
 
 app.use(cors(corsOptions));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.get('/users', (req, res) => {
   res.send(
     data.users.map((user: (typeof data.users)[number]) => {
       return {
@@ -23,6 +40,29 @@ app.get('/', (req, res) => {
   );
 });
 
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+  const valid = validate(newUser);
+  if (!valid) {
+    res.status(400).json({ error: validate.errors });
+  } else {
+    newUser.id = data.users.length + 1;
+    data.users.push(newUser);
+    res.status(201).json(newUser);
+  }
+});
+
+app.delete('/users/:id', (req, res) => {
+  const userId = Number(req.params.id);
+  const userIndex = data.users.findIndex((user) => user.id === userId);
+  if (userIndex === -1) {
+    res.status(404).json({ error: 'User not found' });
+  } else {
+    data.users.splice(userIndex, 1);
+    res.status(204).send();
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Server started at http://localhost:3000`);
+  console.log(`Server started at ${port}`);
 });
